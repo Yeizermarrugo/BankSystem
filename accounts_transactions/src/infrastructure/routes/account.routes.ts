904c4ed44e241ae13bcc53b6e48bba2c598c3550
Yeizermarrugo <yeizermarrugo@gmail.com>
2025-03-06 22:51:45 -0500

    [ADD] another endpoint for the account

--- accounts_transactions/src/infrastructure/routes/account.routes.ts
+++ accounts_transactions/src/infrastructure/routes/account.routes.ts
@@ -5,7 +5,10 @@ import { verifyToken } from "../middleware/auth.middleware";
 const router = Router();
 
 router.post("/", verifyToken, accountController.create);
+router.get("/", verifyToken, accountController.getAll);
+router.get("/:id", verifyToken, accountController.getById);
+router.patch("/:id", verifyToken, accountController.edit);
 
-router.post("/register");
+router.delete("/:id", verifyToken, accountController.remove);
 
 export { router };
