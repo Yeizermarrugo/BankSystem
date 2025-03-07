0f2d76a8017daa24a36f362c5b2098aa111dce07
Yeizermarrugo <yeizermarrugo@gmail.com>
2025-03-06 22:54:33 -0500

    [FIX] Fix trigger to update balance

--- accounts_transactions/src/domain/model/trigger.ts
+++ accounts_transactions/src/domain/model/trigger.ts
@@ -2,23 +2,32 @@ import db from "../utils/database";
 
 async function createTriggers() {
 	await db.query(`
-    CREATE OR REPLACE FUNCTION actualizar_saldo()
-    RETURNS TRIGGER AS $$
-    BEGIN
-        IF NEW.tipo = 'ingreso' THEN
-            UPDATE accounts SET saldo = saldo + NEW.monto WHERE id = NEW.accountId;
-        ELSIF NEW.tipo = 'egreso' THEN
-            UPDATE accounts SET saldo = saldo - NEW.monto WHERE id = NEW.accountId;
-        END IF;
-        RETURN NEW;
-    END;
-    $$ LANGUAGE plpgsql;
+        DROP TRIGGER IF EXISTS trigger_actualizar_saldo ON transactions;
+        DROP FUNCTION IF EXISTS actualizar_saldo();
+        -- Espera un poco para asegurarse de que el trigger y función se eliminen correctamente.
+    `);
 
-    CREATE TRIGGER trigger_actualizar_saldo
-    AFTER INSERT ON transactions
-    FOR EACH ROW
-    EXECUTE FUNCTION actualizar_saldo();
-  `);
+	setTimeout(async () => {
+		await db.query(`
+            CREATE OR REPLACE FUNCTION actualizar_saldo()
+            RETURNS TRIGGER AS $$
+            BEGIN
+                IF NEW."tipo" = 'ingreso' THEN
+                    UPDATE accounts SET saldo = saldo + NEW."monto" WHERE id = NEW."accountId";
+                ELSIF NEW."tipo" = 'egreso' THEN
+                    UPDATE accounts SET saldo = saldo - NEW."monto" WHERE id = NEW."accountId";
+                END IF;
+    
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+    
+            CREATE TRIGGER trigger_actualizar_saldo
+            AFTER INSERT ON transactions
+            FOR EACH ROW
+            EXECUTE FUNCTION actualizar_saldo();
+        `);
+	}, 500);
 }
 
 export default createTriggers;
